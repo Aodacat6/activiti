499127ce5ec0e4649840ae0f0a28be5f3e249609
songdalin <songdalin@qdcares.cn>
2022-04-03 10:14:36 +0800

    基础

---
+++ src/main/java/com/mycom/activiti/ActivitiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiStudyApplication.class, args);
+	}
+
+}
